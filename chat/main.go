9da7180cabe9e8888df00a995be348d05caa0c7d
togatoga <togasakitogatoga@gmail.com>
2016-11-12 20:56:02 +0900

    Commit

--- chat/main.go
+++ chat/main.go
@@ -9,6 +9,10 @@ import (
 	"flag"
 	"../trace"
 	"os"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -28,9 +32,17 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+	gomniauth.SetSecurityKey("セキュリティキー")
+	gomniauth.WithProviders(
+		facebook.New()
+	github.New()
+	google.New()
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename:"chat.html"})
+	http.Handle("/", MustAuth(&templateHandler{filename:"chat.html"}))
+	http.Handle("/login", &templateHandler{filename:"login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 	log.Println("Webサーバーを開始します。ポート:", *addr)
