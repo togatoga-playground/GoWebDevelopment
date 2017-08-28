0385d57eed9015cb9327c232e559d7e2b62382ae
togatoga <togasakitogatoga@gmail.com>
2017-08-28 21:14:14 +0900

    Impl loginHandler

--- chat/auth.go
+++ chat/auth.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/http"
+	"strings"
+	"log"
+	"fmt"
 )
 
 type authHandler struct {
@@ -21,4 +24,18 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request)  {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
 }
