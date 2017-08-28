1046593137b5f00d0b2b5805bf6d091e27b9c8cb
togatoga <togasakitogatoga@gmail.com>
2017-08-28 21:52:45 +0900

    Impl getAuthConfigs

--- chat/main.go
+++ chat/main.go
@@ -8,6 +8,11 @@ import (
 	"sync"
 	"mycode/trace"
 	"os"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
 )
 
 type templateHandler struct {
@@ -23,11 +28,38 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, nil)
 }
 
+type authConfig struct {
+	service string `json:"service"`
+	clientId string `json:"client_id"`
+	secret string `json:"secret"`
+}
+
+
+func getAuthConfigs() []authConfig {
+	bytes, err := ioutil.ReadFile("config/auth.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var authConfigs []authConfig
+	if err := json.Unmarshal(bytes, &authConfigs); err != nil {
+		log.Fatal(err)
+	}
+	return authConfigs
+}
+
+
+
 func main() {
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
+	authConfigs := getAuthConfigs()
+	fmt.Println(authConfigs)
+	gomniauth.SetSecurityKey("togatogatogatogatogatoga")
+	
 
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename:"login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 	if err := http.ListenAndServe(":8080", nil); err != nil {
