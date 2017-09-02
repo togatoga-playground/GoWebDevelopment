f08008525067ec3786fd64563e80dd4b0591735c
togatoga <togasakitogatoga@gmail.com>
2017-09-02 23:21:32 +0900

    Add avatar_test.go

--- chat/avatar.go
+++ chat/avatar.go
@@ -7,3 +7,16 @@ var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できませ�
 type Avatar interface {
 	GetAvatarURL(c *client) (string, error)
 }
+type AuthAvatar struct {
+}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
