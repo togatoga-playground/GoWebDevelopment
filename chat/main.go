37fbffebbe5840929268631281e7bf07fc2bbcf7
togatoga <togasakitogatoga@gmail.com>
2017-09-04 21:45:09 +0900

    Add upload.html

--- chat/main.go
+++ chat/main.go
@@ -69,17 +69,17 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
-			Name:"auth",
-			Value:"",
-			Path:"/",
-			MaxAge:-1,
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
 		})
 		w.Header()["Location"] = []string{"/chat"}
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
-
+	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	go r.run()
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
