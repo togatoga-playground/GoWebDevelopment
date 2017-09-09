e9fed70b2cb303ae0fd59421b2a79cad0cbdd760
togatoga <togasakitogatoga@gmail.com>
2017-09-09 16:28:36 +0900

    Update main.go

--- sprinkle/main.go
+++ sprinkle/main.go
@@ -7,26 +7,26 @@ import (
 	"os"
 	"fmt"
 	"strings"
+	"io/ioutil"
+	"log"
 )
 
 const otherWorld = "*"
 
-var transforms = []string{
-	otherWorld,
-	otherWorld,
-	otherWorld,
-	otherWorld,
-	otherWorld + "app",
-	otherWorld + "site",
-	otherWorld + "time",
-	"get" + otherWorld,
-	"go" + otherWorld,
-	"lets" + otherWorld,
+func readTransforms() []string {
+	b, err := ioutil.ReadFile("config/transforms.conf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Split(string(b), "\n")
 }
 
 func main() {
+	transforms := readTransforms()
+
 	rand.Seed(time.Now().UTC().UnixNano())
 	s := bufio.NewScanner(os.Stdin)
+
 	for s.Scan() {
 		t := transforms[rand.Intn(len(transforms))]
 		fmt.Println(strings.Replace(t, otherWorld, s.Text(), -1))
