588e66b29fb2dbb2b3050e902e276b8eb2d103a2
togatoga <togasakitogatoga@gmail.com>
2017-09-10 21:47:46 +0900

    Update main.go

--- src/sprinkle/main.go
+++ src/sprinkle/main.go
@@ -7,24 +7,11 @@ import (
 	"os"
 	"fmt"
 	"strings"
-	"io/ioutil"
-	"log"
-	"path/filepath"
 )
 
 const otherWorld = "*"
 
-func readTransforms() []string {
-	b, err := ioutil.ReadFile(filepath.Dir(os.Args[0]) + "/config/transforms.conf")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return strings.Split(string(b), "\n")
-}
-
 func main() {
-	transforms := readTransforms()
-
 	rand.Seed(time.Now().UTC().UnixNano())
 	s := bufio.NewScanner(os.Stdin)
 
