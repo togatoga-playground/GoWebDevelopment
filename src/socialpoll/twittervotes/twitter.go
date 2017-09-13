ac9dbce505027f9c73a562d60d6945be08105bd5
togatoga <togasakitogatoga@gmail.com>
2017-09-13 22:53:08 +0900

    Add setupTwitterAuth to twitter.go

--- src/socialpoll/twittervotes/twitter.go
+++ src/socialpoll/twittervotes/twitter.go
@@ -4,6 +4,9 @@ import (
 	"net"
 	"io"
 	"time"
+	"github.com/matryer/go-oauth/oauth"
+	"github.com/joeshaw/envdecode"
+	"log"
 )
 
 var conn net.Conn
@@ -14,7 +17,7 @@ func dial(netw, addr string) (net.Conn, error) {
 		conn = nil
 	}
 
-	netc, err := net.DialTimeout(netw, addr, 5 * time.Second)
+	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
 	if err != nil {
 		return nil, err
 	}
@@ -30,5 +33,33 @@ func closeConn() {
 	}
 	if reader != nil {
 		reader.Close()
+	}
+}
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY",required`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET",required`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN",required`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET",required`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
 	}
 }
