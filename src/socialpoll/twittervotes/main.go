89b09ca55c3c629006d80b158b1d4b9343d86236
togatoga <togasakitogatoga@gmail.com>
2017-09-15 08:04:46 +0900

    Add mgo session to main.go

--- src/socialpoll/twittervotes/main.go
+++ src/socialpoll/twittervotes/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイアル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb()  {
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
+
 func main() {
 }
+
+
