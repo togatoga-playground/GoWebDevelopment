032349550287f16510adbae2c37fdf958038cd5a
togatoga <togasakitogatoga@gmail.com>
2017-09-16 16:56:48 +0900

    Add readFromTwitter to twitter.go

--- src/socialpoll/twittervotes/twitter.go
+++ src/socialpoll/twittervotes/twitter.go
@@ -11,6 +11,8 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
+	"encoding/json"
 )
 
 var conn net.Conn
@@ -70,24 +72,69 @@ func setupTwitterAuth() {
 
 var (
 	authSetupOnce sync.Once
-	httpClient *http.Client
+	httpClient    *http.Client
 )
 
 func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
 	authSetupOnce.Do(func() {
 		setupTwitterAuth()
 		httpClient = &http.Client{
-			Transport:&http.Transport{
-				Dial:dial,
+			Transport: &http.Transport{
+				Dial: dial,
 			},
 		}
 	})
 	formEnc := params.Encode()
 	req.Header.Set("Content-Type", "application/x-www-from-urlencoded")
 	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
-	req.Header.Set("Authorization",authClient.AuthorizationHeader(creds, "POST",req.URL, params))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
 
 	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string)  {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("選択肢の読み込みに失敗しました:", err)
+		return
+	}
+	u, err := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("URLの解析に失敗しました:", err)
+		return
+	}
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	if err != nil {
+		log.Println("検索のリクエストの作成に失敗しました:", err)
+		return
+	}
+
+	resp, err := makeRequest(req, query)
+	if err != nil {
+		log.Println("検索のリクエストの作成に失敗しました:", err)
+		return
+	}
+	reader = resp.Body
+	decoder := json.NewDecoder(reader)
+	for {
+		var tweet tweet
+		if err := decoder.Decode(&tweet); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(strings.ToLower(tweet.Text), strings.ToLower(option)) {
+				log.Println("投票:", option)
+				votes <- option
+			}
+		}
+	}
+
 }
 
 
