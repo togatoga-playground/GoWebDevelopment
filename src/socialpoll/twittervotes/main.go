e641e311f95fe16380a5a9b761c91179b95e5289
togatoga <togasakitogatoga@gmail.com>
2017-09-16 17:25:24 +0900

    Add publishVotes to main.go

--- src/socialpoll/twittervotes/main.go
+++ src/socialpoll/twittervotes/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"gopkg.in/mgo.v2"
 	"log"
+	"github.com/bitly/go-nsq"
 )
 
 var db *mgo.Session
@@ -33,6 +34,22 @@ func loadOptions() ([]string, error) {
 	iter.Close()
 	return options, iter.Err()
 }
+
+func publishVotes(votes <- chan string) <- chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+	go func() {
+		for vote := range votes {
+			pub.Publish("votes", []byte(vote))
+		}
+		log.Println("Publisher: 停止中です")
+		pub.Stop()
+		log.Println("Publisher: 停止しました")
+		stopchan <- struct{}{}
+	}()
+	return stopchan
+}
+
 
 func main() {
 }
