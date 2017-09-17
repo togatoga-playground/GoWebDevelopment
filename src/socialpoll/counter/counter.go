f1c823db07d895fea121adf225d7abb0ab6c5a66
togatoga <togasakitogatoga@gmail.com>
2017-09-17 21:29:12 +0900

    Add counter.go

--- src/socialpoll/counter/counter.go
+++ src/socialpoll/counter/counter.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"flag"
+	"os"
+)
+
+var fatalErr error
+
+func fatal(e error)  {
+	fmt.Println(e)
+	flag.PrintDefaults()
+	fatalErr = e
+}
 func main() {
+	defer func() {
+		if fatalErr != nil {
+			os.Exit(1)
+		}
+	}()
 }
