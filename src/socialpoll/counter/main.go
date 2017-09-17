aa75064b7420b6d911d5dad21a9dd4e56af8c6f7
togatoga <togasakitogatoga@gmail.com>
2017-09-17 21:47:34 +0900

    Add listen to nsq

--- src/socialpoll/counter/main.go
+++ src/socialpoll/counter/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"flag"
 	"os"
+	"log"
+	"gopkg.in/mgo.v2"
+	"sync"
+	"github.com/bitly/go-nsq"
+	"time"
 )
 
 var fatalErr error
@@ -13,13 +18,49 @@ func fatal(e error)  {
 	flag.PrintDefaults()
 	fatalErr = e
 }
+var countsLock sync.Mutex
+var counts map[string]int
+
 func main() {
 	defer func() {
 		if fatalErr != nil {
 			os.Exit(1)
 		}
 	}()
+	log.Println("データベースに接続します...")
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+	defer func() {
+		log.Println("データベース接続を閉じます...")
+		db.Close()
+	}()
 
-	
+	pollData := db.DB("ballots").C("polls")
+
+	log.Println("NSQに接続します...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error{
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
+
+
 
 }
