a8aafabd291cf61255890bdcee9477c400d5f820
togatoga <togasakitogatoga@gmail.com>
2017-09-18 21:53:36 +0900

    Add handlePolls to poll.go

--- src/socialpoll/api/polls.go
+++ src/socialpoll/api/polls.go
@@ -1,10 +1,28 @@
 package main
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
+)
 
 type poll struct {
 	ID bson.ObjectId `bson:"_id" json:"id"`
 	Title string `json:"title"`
 	Options []string `json:"options"`
 	Results map[string]int `json:"results,omitempty"`
-} 
+}
+
+func handlePolls(w http.ResponseWriter, r *http.Request)  {
+	switch r.Method {
+	case "GET":
+		handlePollsGet(w, r)
+		return
+	case "POST":
+		handlePollsPost(w, r)
+		return
+	case "DELETE":
+		handlePollsDelete(w, r)
+		return
+	}
+	respondErr(w, r , http.StatusNotFound)
+}
