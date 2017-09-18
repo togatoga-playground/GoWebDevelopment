3b31da9b9e0a74f25b96fba0f04ac25db9776073
togatoga <togasakitogatoga@gmail.com>
2017-09-18 16:47:53 +0900

    Add main.go

---
+++ src/socialpoll/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+	"gopkg.in/mgo.v2"
+)
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !isValidAPIKey(r.URL.Query().Get("key")) {
+			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		thisDb := d.Copy()
+		defer thisDb.Close()
+		SetVar(r, "db", thisDb.DB("ballots"))
+		f(w, r)
+	}
+}
+
+func withVars(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		OpenVars(r)
+		defer CloseVars(r)
+		fn(w, r)
+	}
+}
+
+func withCORS(fn http.HandlerFunc, ) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
+}
+
+func main() {
+
+}
