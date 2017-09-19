edfc35e68200e6d1fcd8f1bb6af995087b48c156
togatoga <togasakitogatoga@gmail.com>
2017-09-19 20:04:58 +0900

    Impl handlePollsGet

--- src/socialpoll/api/polls.go
+++ src/socialpoll/api/polls.go
@@ -4,6 +4,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
 	"errors"
+	"gopkg.in/mgo.v2"
 )
 
 type poll struct {
@@ -29,5 +30,28 @@ func handlePolls(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlePollsGet(w http.ResponseWriter, r *http.Request) {
+	db := GetVar(r, "db").(*mgo.Database)
+	c := db.C("polls")
+	var q *mgo.Query
+	p := NewPath(r.URL.Path)
+	if p.HasID() {
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		q = c.Find(nil)
+	}
+
+	var result []*poll
+	if err := q.All(&result); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+		return
+	}
+	respond(w, r, http.StatusOK, &result)
+}
+
+func handlePollsPost(w http.ResponseWriter, r * http.Request)  {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
+
+func handlePollsDelete(w http.ResponseWriter, r *http.Request)  {
 	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
 }
