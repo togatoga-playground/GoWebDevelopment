ca3a059eba87e676053a04d5c978f45f4b2be29f
togatoga <togasakitogatoga@gmail.com>
2017-09-19 20:49:28 +0900

    Fix typo

--- src/socialpoll/web/main.go
+++ src/socialpoll/web/main.go
@@ -10,7 +10,7 @@ func main()  {
 	var addr = flag.String("addr", ":8081", "Webサイトのアドレス")
 	flag.Parse()
 	mux := http.NewServeMux()
-	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir())))
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
 	log.Println("Webサイトのアドレス:", *addr)
 	http.ListenAndServe(*addr, mux)
 }
