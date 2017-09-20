b0a91f6313e269dffef53d20eea9c0765e57e6a6
togatoga <togasakitogatoga@gmail.com>
2017-09-20 21:38:34 +0900

    Add archiver.go

--- src/backup/archiver.go
+++ src/backup/archiver.go
@@ -1,4 +1,53 @@
-package main
+package backup
 
-func main() {
+import (
+	"os"
+	"path/filepath"
+	"archive/zip"
+	"io"
+)
+
+type Archiver interface {
+	Archive(src, dest string) error
 }
+
+type zipper struct {}
+
+var ZIP Archiver = (*zipper)(nil)
+
+func (z *zipper) Archive(src, dest string) error {
+	if err := os.Mkdir(filepath.Dir(dest), 0777); err != nil {
+		return err
+	}
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+	w := zip.NewWriter(out)
+	defer w.Close()
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+		in, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		defer in.Close()
+		f, err := w.Create(path)
+		if err != nil {
+			return err
+		}
+
+		io.Copy(f, in)
+		return nil
+	})
+}
+
