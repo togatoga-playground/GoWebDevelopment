deac47a2f59f5b1784ef003f5c00d315518a53e9
togatoga <togasakitogatoga@gmail.com>
2017-09-26 18:10:10 +0900

    Update query.go

--- src/meander/query.go
+++ src/meander/query.go
@@ -2,9 +2,26 @@ package meander
 
 type Place struct {
 	*googleGeometry `json:"geometry"`
-	Name string `json:"name"`
-	Icon string `json:"icon"`
-	Photos[]*googlePhoto `json:"photos"`
-	Vicinity string `json:"vicinity"`
+	Name            string         `json:"name"`
+	Icon            string         `json:"icon"`
+	Photos          []*googlePhoto `json:"photos"`
+	Vicinity        string         `json:"vicinity"`
 }
 
+type googleResponse struct {
+	Results []*Place `json:"results"`
+}
+
+type googleGeometry struct {
+	*googleLocation `json:"location"`
+}
+
+type googleLocation struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+type googlePhoto struct {
+	PhotoRef string `json:"photo_reference"`
+	URL      string `json:"url"`
+}
