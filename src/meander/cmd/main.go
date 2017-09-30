ba05fa2b6024fbb394794b719f71036a4483d74f
togatoga <togasakitogatoga@gmail.com>
2017-09-30 20:29:20 +0900

    Add /recommendations to HandleFunc

--- src/meander/cmd/main.go
+++ src/meander/cmd/main.go
@@ -5,12 +5,24 @@ import (
 	"meander"
 	"net/http"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
+	})
+	http.HandleFunc("/recommendations", func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		places := q.Run()
+		respond(w, r, places)
 	})
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
