c25f0182ea0b246fe2a45d0b2c4ad6ac574a9b48
togatoga <togasakitogatoga@gmail.com>
2017-09-30 19:51:39 +0900

    Add costStrings to query.go

--- src/meander/cost_level.go
+++ src/meander/cost_level.go
@@ -9,4 +9,21 @@ const (
 	Cost3
 	Cost4
 	Cost5
-) 
+)
+
+var costStrings = map[string]Cost{
+	"$":     Cost1,
+	"$$":    Cost2,
+	"$$$":   Cost3,
+	"$$$$":  Cost4,
+	"$$$$$": Cost5,
+}
+
+func (l Cost) String() string {
+	for s, v := range costStrings {
+		if l == v {
+			return s
+		}
+	}
+	return "不正な値です"
+}
